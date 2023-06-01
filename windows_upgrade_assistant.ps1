df8cedd1ec1e8e11c7e10b6ec6cab9393d53adca
wmmatt <87339922+wmmatt@users.noreply.github.com>
2023-06-01 15:33:18 -0500

    Create windows_upgrade_assistant.ps1

---
+++ windows_upgrade_assistant.ps1
@@ -0,0 +1,29 @@
+# Set TLS
+[Net.ServicePointManager]::SecurityProtocol = [Enum]::ToObject([Net.SecurityProtocolType], 3072)
+
+# Set / create dir
+$dir = "$env:SystemDrive\Windows\LTSvc\packages"
+New-Item $dir -ErrorAction SilentlyContinue | Out-Null
+$file = "$($dir)\WindowsUpgrade.exe"
+
+# Get the correct Upgrade Assistant
+$os = (Get-ComputerInfo).OSName
+switch -Wildcard ($os) {
+    '*11*' { $url = 'https://go.microsoft.com/fwlink/?linkid=2171764 ' }
+    '*10*' { $url = 'https://go.microsoft.com/fwlink/?LinkID=799445 ' }
+    Default { Return 'Unsupported OS' }
+}
+
+# Delete existing upgrader if it exists
+Remove-Item $file -Force -ErrorAction SilentlyContinue
+
+# Download Upgrade Assistant
+$webClient = New-Object System.Net.WebClient
+$webClient.DownloadFile($url,$file)
+
+# Install latest Windows 10/11
+Start-Process -FilePath $file -ArgumentList '/quietinstall /skipeula /auto upgrade /copylogs $dir'
+
+<#
+%windir%\System32\WindowsPowerShell\v1.0\powershell.exe -Command "&{ [Net.ServicePointManager]::SecurityProtocol = [Enum]::ToObject([Net.SecurityProtocolType], 3072); $dir = \"%windir%\LTSvc\packages\"; New-Item $dir -ItemType Directory -ErrorAction SilentlyContinue | Out-Null; $webClient = New-Object System.Net.WebClient; $os = (Get-ComputerInfo).OSName; switch -Wildcard ($os) { '*11*' { $url = 'https://go.microsoft.com/fwlink/?linkid=2171764 '; 'Detected Windows 11' }; '*10*' { $url = 'https://go.microsoft.com/fwlink/?LinkID=799445 '; 'Detected Windows 10' }; Default { Return 'Unsupported OS' }}; $file = \"$dir\WindowsUpgrade.exe\"; Remove-Item $file -Force -ErrorAction SilentlyContinue; $webClient.DownloadFile($url,$file); 'Beginning Windows Upgrade...'; Start-Process -FilePath $file -ArgumentList '/quietinstall /skipeula /auto upgrade /copylogs $dir' }"
+#>
